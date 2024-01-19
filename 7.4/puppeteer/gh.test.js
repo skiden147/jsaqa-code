0f2a33b12a3759085ca66f5edaa19ed7039858ff
Denis Skidan <skidan147@gmail.com>
2024-01-19 16:19:34 +0300

    "Второе задание выполнено"

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
 	page = await browser.newPage();
-	await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,6 +9,10 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+	beforeEach(async () => {
+		await page.goto("https://github.com/team");
+	});
+
 	test("The h1 header content'", async () => {
 			const firstLink = await page.$("header div div a");
 			await firstLink.click();
@@ -37,4 +40,29 @@ describe("Github page tests", () => {
 			expect(actual).toContain("Sign up for free")
 		},
 		5000);
+
 });
+
+test("The title on Github Education page", async () => {
+		await page.goto("https://education.github.com/");
+		const actual = await page.title();
+		expect(actual).toEqual("Engaged students are the result of using real-world tools - GitHub Education");
+	},
+	4000);
+
+test("The title on Github Enterprise page", async () => {
+		await page.goto("https://github.com/enterprise");
+		const actual = await page.title();
+		expect(actual).toEqual("The AI Powered Developer Platform. · GitHub");
+	},
+	4000);
+
+test("The title on Github Startups page", async () => {
+		await page.goto("https://github.com/enterprise/startups");
+		const actual = await page.title();
+		expect(actual).toEqual("GitHub for Startups: Build your startup on GitHub · GitHub");
+	},
+	3000);
+
+
+
